44911414c1850ac738e35c69aa8a98bc54e28de5
dimka665 <dimka665@gmail.com>
2013-07-09 07:22:29 +0400

    setup.py improvement

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
 import os
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 root = os.path.dirname(os.path.realpath(__file__))
 long_description = open(os.path.join(root, 'README.rst')).read()
 
 setup(
     name='range-regex',
-    version='1.0.2',
+    version='1.0.3',
     description='Python numeric range regular expression generator',
     long_description=long_description,
     url='http://github.com/dimka665/range-regex',
     author='Dmitry Voronin',
     author_email='dimka665@gmail.com',
-    license='Python Software Foundation License',
-    packages=['range_regex'],
-    platforms=['any'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Python Software Foundation License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
+    license='BSD',
+    # packages=['range_regex'],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords='numeric range regex regular expression generator',
 )
